eb71763bfabc945fe0771bf517e07e004bba9c3c
wyatt4543 <w4543w@gmail.com>
2026-03-03 11:35:56 -0600

    finish functions for updating assignments

--- Assignment Tracker Application/model/assignment_db.php
+++ Assignment Tracker Application/model/assignment_db.php
@@ -46,5 +46,25 @@ function add_assignment($course_id, $description)
 
 function update_assignment($assignment_id, $description, $course_id)
 {
+    global $db;
+    $query = 'UPDATE assignments 
+              SET Description = :description, courseID = :course_id 
+              WHERE ID = :assignment_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':assignment_id', $assignment_id, PDO::PARAM_INT);
+    $statement->bindValue(':description', $description, PDO::PARAM_STR);
+    $statement->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+    $statement->execute();
+    $statement->closeCursor();
+}
 
+function get_assignment($assignment_id) {
+    global $db;
+    $query = 'SELECT * FROM assignments WHERE ID = :assignment_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':assignment_id', $assignment_id, PDO::PARAM_INT);
+    $statement->execute();
+    $assignment = $statement->fetch();
+    $statement->closeCursor();
+    return $assignment;
 }
